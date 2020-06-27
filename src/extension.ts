8939c967107a12ddc12d8384d1d8fff7f4d17cb9
Lukas Spieß <lukas.spiess@gmail.com>
2020-06-27 16:06:17 +0200

    Fix Promise-related lint warnings

--- src/extension.ts
+++ src/extension.ts
@@ -3,17 +3,20 @@ import { generateSnippet } from './lib/textHelpers';
 
 export function activate(context: ExtensionContext): void {
 	context.subscriptions.push(
-		commands.registerTextEditorCommand('snippet-copy.copySnippet', async (editor) => {
-			const snippet = await generateSnippet(editor.document, editor.selections, false);
-
-			void env.clipboard.writeText(snippet);
+		commands.registerTextEditorCommand('snippet-copy.copySnippet', (editor) => {
+			void generateSnippet(editor.document, editor.selections, false)
+				.then((snippet) => {
+					return env.clipboard.writeText(snippet);
+				});
 		})
 	);
-	context.subscriptions.push(
-		commands.registerTextEditorCommand('snippet-copy.copySnippetAsMarkdownCodeBlock', async (editor) => {
-			const snippet = await generateSnippet(editor.document, editor.selections, true);
 
-			void env.clipboard.writeText(snippet);
+	context.subscriptions.push(
+		commands.registerTextEditorCommand('snippet-copy.copySnippetAsMarkdownCodeBlock', (editor) => {
+			void generateSnippet(editor.document, editor.selections, true)
+				.then((snippet) => {
+					return env.clipboard.writeText(snippet);
+				});
 		})
 	);
 }
